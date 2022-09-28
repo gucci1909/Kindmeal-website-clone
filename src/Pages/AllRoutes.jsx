d442e19544db9119ac355b57a38a7921aebf3e1b
gucci1909 <umangarora0134@gmail.com>
2022-09-28 23:27:22 +0530

    kindmeal day-2

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
+import Product from './Product'
 
 function AllRoutes() {
   return (
     <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/product" element={<Product/>}></Route>
     </Routes>
   )
 }
